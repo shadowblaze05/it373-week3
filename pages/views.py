80b88748cb2902791aca26031bd4d21a972457e5
shadowblaze05 <shadowblaze259@gmail.com>
2025-09-25 17:36:00 +0800

    first commit

---
+++ pages/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    context = {
+        'title': 'Home Page',
+        'features': ['Django', 'Templates', 'Static Files']
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About Us'})
+
+def hello(request, name):
+    return render(request, 'hello.html', {'name': name})
+
+def  gallery(request):
+    images = [
+        'image1.jpg',
+        'image2.jpg',
+        'image3.jpg',
+    ]
+    return render(request, 'gallery.html', {'images': images})
+
+def page_not_found_view(request, exception):
+    return render(request, '404.html', status=404)
+
+def server_error_view(request):
+    return render(request, '500.html', status=500)
